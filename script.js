36ec8351f445dcc3366b64dc3a2adfe627f75374
Bence <bence4436@gmail.com>
2020-11-23 17:43:05 +0100

    Submit function created in js

---
+++ script.js
@@ -0,0 +1,12 @@
+function Submit(){
+    var matekIrasbeli = parseInt(document.getElementById("irasbeliMat").value);
+    var matekSzobeli = parseInt(document.getElementById("szobeliMat").value);
+    var toriIrasbeli = parseInt(document.getElementById("irasbeliTor").value);
+    var toriSzobeli = parseInt(document.getElementById("szobeliTor").value);
+    var magyarIrasbeli = parseInt(document.getElementById("irasbeliMagy").value);
+    var magyarSzobeli = parseInt(document.getElementById("szobeliMagy").value);
+    var nyelvIrasbeli = parseInt(document.getElementById("irasbeliNyelv").value);
+    var nyelvSzobeli = parseInt(document.getElementById("szobeliNyelv").value);
+
+    
+}
